97f2a9a49ae90d8d5e54fa4bcd4a4245719e75d7
Pickachu Chen <pickachu1006@gmail.com>
2020-11-27 10:19:35 +0800

    20201127_php web test

---
+++ index_fac05.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<!-- meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+	<meta name="format-detection" content="telephone=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="%DESCRIPTION%">
+	<meta name="keywords" content="%KEYWORDS%">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:image" content="">
+	<meta property="og:title" content="“副乳”のない美しい胸…私たちは諦められない！優しく守って美胸になれる『Viageナイトブラ』は絶対着けて！">
+	<meta property="og:description" content="%DESCRIPTION%">
+<!-- title -->
+    <title>“副乳”のない美しい胸…私たちは諦められない！優しく守って美胸になれる『Viageナイトブラ』は絶対着けて！</title>
+<!-- css -->
+    <link rel="stylesheet" media="all" href="css/style.css">
+<!-- js -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '3072816372822384');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=3072816372822384&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+</head>
+<body>
+
+<!--HTML Start-->
+
+<div id="wrapper">
+<div id="container">
+    <header>
+         <div><img src="img/header_01.png" alt="beauty News"></div>
+         <h1>“副乳”のない美しい胸…私たちは諦められない！優しく守って美胸になれる『Viageナイトブラ』は絶対着けて！</h1>
+         <div><img src="img/fv_01.png" alt="優しく守って美胸になれる『Viageナイトブラ』"></div>
+	</header>
+
+    <main>
+        <section class="txtBox_670">
+            <p>冬、コートを脱ぐと気になるのが<br><span class="blue big01 bold">キレイに見えない私の胸…TT</span><br><span class="bold gray small01">特にニット系の服を着ていると本当に</span></p>
+            <div><img src="img/img_01.png" alt="キレイに見えない私の胸"></div>
+            <p>ボディラインの出る服がどうも似合わなくて<br>胸が小さいから諦めていたんです、でも<br><span class="bold blue big01">原因は胸の形が良くないことだったんです！</span></p>
+            <div><img class="pb_0" src="img/img_02.png" alt="副乳・分厚い背肉・広がった胸、現れない谷間"></div>
+            <p class="txt_c">↑↑↑<br>これらのことの原因はまさに<br><span class="bold blue big02">胸の型がよくない！</span><br><span class="bold gray small01">胸のサイズより形のほうがスタイルには重要らしい<br>みなさん知ってましたか～～</span></p>
+            <p>そんな私の胸を美しく整えてくれたのが</p>
+            <div><img class="pb_0" src="img/img_03.png" alt="『Viageナイトブラ』"></div>
+            <p class="notes">※補正効果によるもの</p>
+            <p ontouchstart=""><span class="bold pink big02">美胸維持のために特殊設計された<br>『<a href="https://viagebeautybra.com/vgma01/?utm_source=spt&utm_medium=facebook&utm_campaign=cu02_bn1_at_BR" target="_blank">Viageナイトブラ</a>』！</span></p>
+            <div><img src="img/img_04.png" alt="7色から選べる『Viageナイトブラ』"></div>
+            <p>可愛い！7色から選べる～<br><span class="bold">深い谷間も出来たし、背中もスッキリ！</span></p>
+            <div><img src="img/img_05.png" alt="胸大きくなった？って言われるように"></div>
+            <p>Viageナイトブラを着け始めてから<br>胸大きくなった？って言われるようになった～</p>
+        </section>
+
+        <section class="txtBox_670">
+            <h2>3ポイントで美胸へ導く！<br>寝ている間に楽チン胸ケア</h2>
+            <div><img src="img/img_06.png" alt="7色から選べる『Viageナイトブラ』"></div>
+            <p>Viageナイトブラは普通のブラとは違います<br><span class="bold">特殊な製法で伸縮性のある生地を使用している</span>ので<br><span class="bold pink big01">背中や脇の肉を寄せて<br>ボディラインを綺麗にみせてくれます！</span></p>
+            <div><img class="pb_0" src="img/img_07.png" alt="ボディラインを綺麗にみせてくれる『Viageナイトブラ』"></div>
+            <p class="imgTtl">美胸維持！<br>Viage晩安内衣のここがすごい</p>
+            <div><img class="pb_0" src="img/img_08.png" alt="Point1　機能性が高い！"></div>
+            <div><img class="pb_0" src="img/img_09.gif" alt="Point2　伸縮性がいい！"></div>
+            <div><img src="img/img_10.png" alt="Point3　使い心地がいい！"></div>
+            <p>Viageナイトブラは胸の形をキレイに整える機能が本当にすごいんです！</p>
+            <p><span class="check">しっかり持ちあげて<span class="bold">上向きの自然な胸の膨らみを実現</span></span><br><span class="check">胸を守って<span class="bold">谷間をつくる</span></span><br><span class="check">流れていく<span class="bold">背中の肉を胸に誘導する</span></span><br><span class="check"><span class="bold">よく伸縮</span>して快適に眠れる</span><br><span class="check"><span class="bold">洗濯表示はブラジャーの内側に縫い付けてあって</span>気にならない</span><br><span class="check">生地の<span class="bold">品質が非常に良い</span></span></p>
+        </section>
+
+        <section class="txtBox_670">
+            <h2>どうしてViageナイトブラがいいですか？</h2>
+            <div><img src="img/img_11.png" alt="Viageナイトブラがいい理由"></div>
+            <p><span class="bold">寝るときに何も着けないのは最もよくない</span>ですが<br><span class="bold blue big02">機能性がないブラは着けても意味がないです！</span><br>悪い習慣を続けると胸の形はどんどん変わっていきます。</p>
+            <div><img src="img/img_12.png" alt="悪い習慣を続けると胸の形はどんどん変わっていく"></div>
+            <p>寝ている間に背中や脇に流れる私の胸たち… 戻ってきて…</p>
+            <p class="pt_1"><span class="bold pink big01">Viageナイトブラは機能性に特化したシンプルなデザイン</span></p>
+            <div><img src="img/img_13.png" alt="機能性に特化したシンプルなデザイン"></div>
+            <p><span class="bold">体にストレスを与えるものは全て排除</span>して、<br>伸縮性の良い高品質な生地を使っている<br>だから体にとても合います！</p>
+            <p class="txt_c pb_0 pt_1 lh_shrink"><span class="pink">横になると広がるはずの胸がViageナイトブラで持ち上がる！</span></p>
+            <div><img src="img/img_14.png" alt="Viageナイトブラで持ち上がった胸"></div>
+            <p>胸が流れ落ちるのを防ぐ特殊な構造で<br>横になっても深い谷間が現れます！！！</p>
+            <p class="txt_c pb_0 pt_1 lh_shrink"><span class="pink">美しい胸が欲しいなら、寝るときは絶対Viageナイトブラを使いましょう～～</span></p>
+            <div><img src="img/img_15.png" alt="Viageナイトブラ"></div>
+            <p><span class="bold pink big01">しっかり胸を守ってくれるのに、睡眠を邪魔しない！</span><br><span class="bold gray small01">着心地が良いので休みの日もずっとViageを着けています</span></p>
+        </section>
+
+        <section class="txtBox_670">
+            <h2>累計販売枚数300万枚以上！<br>Viageナイトブラは日本で大人気</h2>
+            <div><img src="img/img_16.png" alt="日本で大人気のViageナイトブラ"></div>
+            <p><span class="bold">日本ではモデルにも超人気</span>のナイトブラで<br>人気雑誌に掲載されSNSでも話題になっています</p>
+            <p><span class="bold pink">👩 食い込まないのに胸全体をしっかり支えてくれてる感じ<br>👩 肌触り超いい！<br>👩 何もしないより着けてるほうが気持ち良い<br>👩 今まででこれが一番！<br>👩 今日からヴィアージュで胸の形をキレイにします！</span></p>
+            <p>美ボディでいたい日本の女性たちがみんな愛用しているんですね！</p>
+        </section>
+
+        <section>
+            <div class="txtBox_670">
+                <h2>台湾でも高評価がたくさん！<br>使ってみて効果を感じてください</h2>
+                <div><img src="img/img_17.png" alt="台湾でも高評価がたくさんのViageナイトブラ"></div>
+                <p>女性の胸の悩みに本当に重要なのは<br><span class="bold blue big01">早めに対策をすること！！！</span><br>一度形が壊れたら戻らないんですから…<br><span class="bold pink big02">今から胸ケアを始めればあなたはもっと美しくなれる！</span></p>
+                <div><img src="img/img_18.png" alt="胸ケアならViageナイトブラ"></div>
+                <p>もしみなさん胸に悩みがあるなら、まず使ってみて<br>絶対失望させません！！！</p>
+                <p class="sale">4枚以上の購入で洗濯ネットプレゼント！</p>
+                <div><img class="pt_0" src="img/img_19.png" alt="Viageナイトブラ"></div>
+            </div>
+            <div class="btn" ontouchstart=""><a href="https://viagebeautybra.com/vgma01/?utm_source=spt&utm_medium=facebook&utm_campaign=cu02_bn1_at_BR" target="_blank"><img src="img/btn_01.png" alt="Viage台湾公式サイト"></a></div>
+            <div class="txtBox_670">
+                <p><span class="bold pink big02">『Viageナイトブラ』は日本で注文が殺到している超人気商品</span><br><span class="bold">台湾でも絶賛発売中です！</span></p>
+                <p><span class="bold gray small01">色とサイズを選んだら、毎日の美胸ケアを始めましょう～～</span></p>
+            </div>
+        </section>
+    </main>
+
+	<footer>
+		<div id="copy"><p>HRC Co., Ltd</p></div>
+	</footer>
+</div>
+</div><!-- /#wrapper -->
+
+<!--HTML End-->
+<!-- SiTest Tag -->
+<script src="//sitest.jp/tracking/sitest_js?p=5eb9299b9c5a5" async></script>
+<!-- END SiTest Tag -->
+</body>
+</html>
